532bf17054c4e280757783d17dc40f564a5aa011
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-04 22:11:15 +0800

    update

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -38,7 +38,6 @@
 // global after init google map callback
 // callback
 function initMap(){
-  alert('init');
   if ($('#geocode').length > 0)
     autocomplete = new google.maps.places.Autocomplete($('#geocode')[0]);
 }
@@ -69,13 +68,13 @@ function initImageInput() {
   });
 };
 
-// $(function(){
-//   if (typeof google === 'object' && typeof google.maps === 'object') {
-//     initMap();
-//   } else {
-//     var script = document.createElement('script');
-//     script.type = 'text/javascript';
-//     script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyCUSk-5eNj1T5RH1U6tWPuqzA5fVMv37f0&libraries=places&callback=initMap';
-//     document.body.appendChild(script);
-//   }
-// });
+$(function(){
+  if (typeof google === 'object' && typeof google.maps === 'object') {
+    initMap();
+  } else {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyCUSk-5eNj1T5RH1U6tWPuqzA5fVMv37f0&libraries=places&callback=initMap';
+    document.body.appendChild(script);
+  }
+});
