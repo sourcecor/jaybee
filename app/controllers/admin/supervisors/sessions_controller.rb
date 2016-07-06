dbe76a54719b51ba67ad20770ad1925ab0c66c4e
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-06 11:44:50 +0800

    update

--- app/controllers/admin/supervisors/sessions_controller.rb
+++ app/controllers/admin/supervisors/sessions_controller.rb
@@ -29,7 +29,6 @@ class Admin::Supervisors::SessionsController < Devise::SessionsController
     def check_captcha
       unless verify_recaptcha
         self.resource = resource_class.new sign_in_params
-        flash[:alert] = "圖形驗證不正確"
         respond_with_navigational(resource) { render :new }
       end
     end
