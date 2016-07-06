4662e869e2bbcfa15b96f1459d40c88611e4c7c8
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-06 13:21:26 +0800

    update 20160702

--- db/migrate/20160705064013_create_banners.rb
+++ db/migrate/20160705064013_create_banners.rb
@@ -3,9 +3,7 @@ class CreateBanners < ActiveRecord::Migration
     create_table :banners do |t|
       t.string :caption, limit: 20, default: '#'
       t.integer :parent_id
-      t.string :picture, limit: 50
       t.integer :seq
-
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20160705064013) do
   create_table "banners", force: :cascade do |t|
     t.string   "caption",    limit: 20, default: "#"
     t.integer  "parent_id"
-    t.string   "picture",    limit: 50
     t.integer  "seq"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
