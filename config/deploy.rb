f7b7b9b4fe27589f634d6b71c2d876f132786630
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-11 21:37:38 +0800

    firt commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :linked_dirs, fetch(:linked_dirs, []).push('public/assets', 'public/uploads')
-set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "shared/tmp/pids", "shared/log", "shared/tmp/sockets", "vendor/bundle")
+set :linked_dirs, fetch(:linked_dirs, []).push("bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle")
 set :keep_releases, 5
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
@@ -22,7 +22,7 @@ set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "config/puma.rb"
+set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
