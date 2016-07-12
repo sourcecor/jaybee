380f561b07e666030cc8b44c0e049136d65a01fb
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-12 22:59:28 +0800

    first commit

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -16,7 +16,7 @@ class Menu < ActiveRecord::Base
 
     if (self.action.to_s != "#")
       begin
-        puts send(self.action)
+        puts eval(self.action)
       rescue => ex
         errors.add(:action, "這個路由不存在")
       end
