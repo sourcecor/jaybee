e574f3799bd305d9eb1103fb921b70e4e043b8c6
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-13 08:20:11 +0800

    first commit

--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -16,8 +16,12 @@ module Admin::CategoriesHelper
   def cate_group
     r_cate = []
     Category.where(parent_id: 0).each do |cate|
-      g_cate = cate_recur(cate)
-      r_cate << [ cate.caption, g_cate ]
+      if !cate.sub_categories.empty?
+        g_cate = cate_recur(cate)
+        r_cate << [ cate.caption, g_cate ]
+      else
+        r_cate << [ cate.caption, cate.id ]
+      end
     end
     return r_cate
   end
