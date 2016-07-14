f7307c1b065494e95de97dad1c7cf7c930bb7cc8
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-15 00:07:51 +0800

    Add Chat Module

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ActiveRecord::Base
+  belongs_to :conversation
   belongs_to :user
 end
--- db/migrate/20160714160636_create_messages.rb
+++ db/migrate/20160714160636_create_messages.rb
@@ -2,6 +2,7 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.text :body
+      t.references :conversation, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
