10c287eef732dd536bfc3e72083fab92fc23d15a
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-15 12:40:01 +0800

    init schedule job gem#7s

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,7 @@
 
 # Example:
 #
+set :bundle_command, "/home/ubuntu/.rbenv/shims/bundle"
 set :output, "/home/ubuntu/jaybee/current/log/cron_log.log"
 set :environment, :production
 every 2.minutes do
