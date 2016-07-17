f4f79b6e9ce2318cb6d83f001fb57b5ab18d3553
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-17 11:59:02 +0800

    remove garbage

--- test/controllers/admin/pictures_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::PicturesControllerTest < ActionController::TestCase
-  test "should get update_sequence" do
-    get :update_sequence
-    assert_response :success
-  end
-
-end
--- test/models/sub_product_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SubProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
