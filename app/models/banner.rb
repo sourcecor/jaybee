4b76fe494981d2db4fbeaf400fbeb61dd581884d
Sourcecor103 <Sourcecor103@gmail.com>
2016-07-20 14:08:28 +0800

    fix upload picture config

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -6,4 +6,17 @@ class Banner < ActiveRecord::Base
   accepts_nested_attributes_for :sub_banners, allow_destroy: true, :reject_if => lambda { |a| a[:caption].blank? }
 
   belongs_to :banner, class_name: "Banner"
+
+  before_destroy :remember_id
+  after_destroy :remove_id_directory
+
+  protected
+
+  def remember_id
+    @id = id
+  end
+
+  def remove_id_directory
+    FileUtils.remove_dir("#{Rails.root}/public/uploads/banner/#{@id}", :force => true)
+  end
 end
