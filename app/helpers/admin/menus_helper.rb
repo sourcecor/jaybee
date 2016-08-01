c4fe6bf37d6389842624371f5a0296d046766488
Sourcecor103 <Sourcecor103@gmail.com>
2016-08-01 14:21:38 +0800

    category menus from db.

--- app/helpers/admin/menus_helper.rb
+++ app/helpers/admin/menus_helper.rb
@@ -2,6 +2,7 @@ module Admin::MenusHelper
   def sidebarMenu menus
     r_menu = []
     menus.each { |menu|
+      # With the latest version of Rails you can pass "true" to an association method to remove it from the cache before fetching it.
       if (! menu.sub_menus(true).empty?)
         sub_menus = sidebarMenu menu.sub_menus
         r_menu << {'id'=> menu.id, 'title'=>menu.caption, 'icon'=> menu.icon, 'children'=>sub_menus}
