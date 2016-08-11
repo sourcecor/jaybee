2ca0448dee2df914669c33210b1aae0bf786f7e2
Sourcecor103 <Sourcecor103@gmail.com>
2016-08-12 00:16:49 +0800

    update home grids

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,7 +3,8 @@ class Admin::ProductsController < Admin::ApplicationController
 
   def index
     keyword = params[:product][:caption] unless params[:product].nil?
-    cate_ids = params[:product][:categories].reject!(&:empty?).join(',').split(",").map(&:to_i) unless params[:product].nil?
+    cate_ids = params[:product][:categories].reject(&:empty?).join(',').split(",").map(&:to_i) unless params[:product].nil?
+    puts cate_ids
     @products_grid = initialize_grid(Product.keyword_with(keyword).cates_with(cate_ids).order(id: :desc).distinct)
   end
 
