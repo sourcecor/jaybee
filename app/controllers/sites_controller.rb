b9ae948ea893e43723079f76cca3d0f2258d3a66
Sourcecor103 <Sourcecor103@gmail.com>
2016-08-13 18:44:01 +0800

    update home grids

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -14,12 +14,18 @@ class SitesController < ApplicationController
     @menus = categoryMenu Category.c_parent(cate_id) unless cate_id.nil?
 
     cate_ids = Category.c_cate_ids(cate_id) unless cate_id.nil?
-
-    @products = Product.keyword_with(keyword).cates_with(cate_ids).started_at(Date.today.strftime("%Y-%m-%d %H:%M")).order(id: :desc).distinct.page params[:page]
+    # @products = Product.keyword_with(keyword).cates_with(cate_ids).started_at(Date.today.strftime("%Y-%m-%d %H:%M")).order(id: :desc).distinct.page params[:page]
+    @products = Product.keyword_with(keyword).cates_with(cate_ids).order(id: :desc).distinct.page params[:page]
   end
 
   def show
-    render 'detail', data: Product.find(params[:id])
+    @product = Product.find(params[:id])
+    cate_id = params[:category]
+    cate_id = @product.categories.first.id if params[:category].nil?
+    @c_bread = Category.c_bread(cate_id) unless cate_id.nil?
+    @menus = categoryMenu Category.c_parent(cate_id) unless cate_id.nil?
+
+    render :detail
   end
 
   # static page
