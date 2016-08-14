3a150e6b98940f0c60f2cb20bf69cf838205b0d7
Sourcecor103 <Sourcecor103@gmail.com>
2016-08-14 15:45:11 +0800

    add member function.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -14,8 +14,8 @@ class SitesController < ApplicationController
     @menus = categoryMenu Category.c_parent(cate_id) unless cate_id.nil?
 
     cate_ids = Category.c_cate_ids(cate_id) unless cate_id.nil?
-    # @products = Product.keyword_with(keyword).cates_with(cate_ids).started_at(Date.today.strftime("%Y-%m-%d %H:%M")).order(id: :desc).distinct.page params[:page]
-    @products = Product.keyword_with(keyword).cates_with(cate_ids).order(id: :desc).distinct.page params[:page]
+    @products = Product.keyword_with(keyword).cates_with(cate_ids).started_at(DateTime.now.strftime("%Y-%m-%d %H:%M")).order(id: :desc).distinct.page params[:page]
+    # @products = Product.keyword_with(keyword).cates_with(cate_ids).order(id: :desc).distinct.page params[:page]
   end
 
   def show
