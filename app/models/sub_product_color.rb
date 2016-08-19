7190c236d23cfab78db0bc71f0e33c2d6bbfadf3
Sourcecor103 <Sourcecor103@gmail.com>
2016-08-19 12:05:43 +0800

    add member function.

--- app/models/sub_product_color.rb
+++ app/models/sub_product_color.rb
@@ -7,7 +7,7 @@ class SubProductColor < ActiveRecord::Base
   has_many :pictures, -> { order(:seq) }, as: :imageable, class_name: "Picture", dependent: :destroy
   accepts_nested_attributes_for :pictures, allow_destroy: true
   #產品明細
-  has_many :sub_products, -> { where(:deleted_at => nil).order(:size) }, dependent: :destroy
+  has_many :sub_products, -> { where(:deleted_at => nil).order(:seq, :size) }, dependent: :destroy
   accepts_nested_attributes_for :sub_products, allow_destroy: true
 
   belongs_to :product
