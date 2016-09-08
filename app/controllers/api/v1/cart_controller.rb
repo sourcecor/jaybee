ba8d6762c6d34d71b186519926e85947069d6994
Sourcecor103 <Sourcecor103@gmail.com>
2016-09-08 22:10:30 +0800

    fix header

--- app/controllers/api/v1/cart_controller.rb
+++ app/controllers/api/v1/cart_controller.rb
@@ -4,7 +4,15 @@ class Api::V1::CartController < Api::V1::ApplicationController
   end
 
   def create
-    cart = new Cart()
+    user_carts = current_user.carts.find_by_itemcode(params['itemcode'])
+    if !user_carts
+      current_user.carts.build(itemcode: params['itemcode'], qty: params['qty'], product_id: params['product_id'])
+      current_user.save
+    else
+      user_carts.qty = user_carts.qty + params['qty'].to_i
+      user_carts.save
+    end
+
     render 'response'
   end
 
@@ -13,6 +21,13 @@ class Api::V1::CartController < Api::V1::ApplicationController
   end
 
   def destroy
+    user_carts = current_user.carts.find_by_itemcode(params['itemcode'])
+    user_carts.destroy
+    response 'response'
+  end
 
+  def clear
+    current_user.carts.delete_all
+    redirect_to sites_path
   end
 end
